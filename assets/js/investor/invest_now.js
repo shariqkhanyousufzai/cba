60ddc186b9158f094862b5ffe0804415024ba0b2
Abdul Moiz Baig <amoiz1992@gmail.com>
2021-07-13 21:33:07 +0000

    pay from dollar fix and stripe live key update

--- assets/js/investor/invest_now.js
+++ assets/js/investor/invest_now.js
@@ -634,7 +634,9 @@ $(document).ready(function(){
 	//stripe sca
 // Create an instance of the Stripe object with your publishable API key
     //var stripe = Stripe("pk_test_51HgCEqGBlKCKevlC3VAgXGzdcE7grNHEb5ay7FRF1qrBCgvm7Ggo5JnGleSDTnC84Ik6vdv4W737l2g6f2Rm7bji00ic4lYm91");
-	var stripe = Stripe("pk_live_51HgCEqGBlKCKevlCVy3tjEch8pnrQgOixqhAsf4JCVnoGUSo9s5WkJRJHHO5kSIwjzIYy5ifnvxbjLnkj5PyzcRy00Js8dyNsa");
+	//var stripe = Stripe("pk_live_51HgCEqGBlKCKevlCVy3tjEch8pnrQgOixqhAsf4JCVnoGUSo9s5WkJRJHHO5kSIwjzIYy5ifnvxbjLnkj5PyzcRy00Js8dyNsa")
+//    var stripe = Stripe("pk_test_51HgCEqGBlKCKevlC3VAgXGzdcE7grNHEb5ay7FRF1qrBCgvm7Ggo5JnGleSDTnC84Ik6vdv4W737l2g6f2Rm7bji00ic4lYm91");
+ var stripe = Stripe("pk_live_51HgCEqGBlKCKevlCVy3tjEch8pnrQgOixqhAsf4JCVnoGUSo9s5WkJRJHHO5kSIwjzIYy5ifnvxbjLnkj5PyzcRy00Js8dyNsa");
     var checkoutButton = document.getElementById("checkout-button");
 
     checkoutButton.addEventListener("click", function () {
